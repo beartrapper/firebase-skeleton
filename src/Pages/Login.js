b2e78d4d2c75288761d215602238761dac334e10
Dendolium <dendodendolium@gmail.com>
2022-06-30 16:59:52 +0530

    login functionality added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,33 +1,68 @@
 import { Button } from "../Components/Button";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth } from "../firebase";
 
 export const Login = () => {
-  const onLogin = () => {};
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [errorLoggingIn, setErrorLoggingIn] = useState(false);
+
+  const onLogin = () => {
+    console.log("in");
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("done");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorLoggingIn(true);
+      });
+  };
 
   return (
     <div>
       <Link to="/signup">Signup?</Link>
       <br />
       <br />
-      Email: <input
+      Email:{" "}
+      <input
         type="text"
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => {
+          setEmail(e.target.value);
+          setErrorLoggingIn(false);
+        }}
       />{" "}
       <br />
       Password:{" "}
       <input
         type="password"
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(e) => {
+          setErrorLoggingIn(false);
+          setPassword(e.target.value);
+        }}
       />{" "}
       <br />
       <br />
-      <Button onClickButton={(event) => onLogin} buttonText="Login" />
+      <Button onClickButton={(event) => onLogin(event)} buttonText="Login" />
       <br />
       <br />
       <Link to="/forgot">forgot password? Fret not!</Link>
+      {errorLoggingIn ? (
+        <>
+          <br />
+          <br />
+          There seems to be an error loggin in
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
